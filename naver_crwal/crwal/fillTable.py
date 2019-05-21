8b118cb02b7f55e6495912b18e0317ee9b8e38d3
garden <khjvvv7@gmail.com>
2019-05-21 14:56:14 +0900

    fill the organs table

--- naver_crwal/crwal/fillTable.py
+++ naver_crwal/crwal/fillTable.py
@@ -3,6 +3,23 @@ import sqlite3
 conn = sqlite3.connect("disease2.db")
 cur = conn.cursor()
 
+organ_list=[]
+
 
 with conn:
-    cur.execute("SELECT * FROM disease WHERE organs="가슴")
+    organ_list = cur.execute("SELECT name FROM sqlite_master WHERE type = 'table';").fetchall()
+    #print(organ_list)
+
+    for organ in range(3,195):
+        print(organ_list[organ][0])
+        str = "DELETE FROM [" + organ_list[organ][0] + "]"
+        print(str)
+        print(organ)
+        cur.execute(str)
+        str2 = "INSERT INTO [" + organ_list[organ][0] + "] (id, symtom) SELECT id, symptom FROM disease WHERE organs LIKE '%" + organ_list[organ][0] +"%'"
+        print(str2)
+        cur.execute(str2)
+
+
+    #st = cur.execute("SELECT organs FROM disease WHERE organs LIKE (?)", ('%가슴%',)).fetchall()
+    #print(st)
